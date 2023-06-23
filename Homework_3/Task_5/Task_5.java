7467d9d3ba4daee5f5b998351477c57fe4f52bb2
Sergey Edalov <edalov94@gmail.com>
2023-06-23 19:56:04 +0300

    homework_3

---
+++ Homework_3/Task_5/Task_5.java
@@ -0,0 +1,24 @@
+//package Homeworks.Homework_3.Task_5;
+//
+//public class Task_5 {
+//    /**
+//     * при входном массиве, вернуть массив массивов со всеми перестановками его элементов. (со звездочкой)
+//     * Пример входных данных:
+//     * [1,2,3]
+//     * Пример выходных данных:
+//     * [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+//     */
+//    public static void main(String[] args) {
+//        int[] array = new int[] {1, 2, 3, 4};
+//
+//
+//    }
+//
+//    public static int[] changeElem (int[] array, int memo) {
+//        int[] result = new int [array.length - 1];
+//        int temp;
+//        for (int i = 0; i < array.length; i++) {
+//            temp = array.
+//        }
+//    }
+//}
